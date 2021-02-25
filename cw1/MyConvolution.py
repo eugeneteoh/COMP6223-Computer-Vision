9df8d835a46348cac230e485fa2b732de42cd765
Eugene Teoh <eugenetwc1@gmail.com>
2021-02-25 20:46:13 +0000

    completed convolution

--- cw1/MyConvolution.py
+++ cw1/MyConvolution.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.signal import convolve2d
 
 def convolve(image: np.ndarray, kernel: np.ndarray) -> np.ndarray: 
     """
@@ -11,3 +12,15 @@ def convolve(image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
     :rtype numpy.ndarray
     """
     # Your code here. You'll need to vectorise your implementation to ensure it runs # at a reasonable speed.
+    kernel = np.flip(kernel)
+
+    cimage = np.zeros(image.shape)
+    rows, cols = image.shape
+    trows, tcols = kernel.shape
+    tr, tc = trows//2, tcols//2
+
+    for x in range(tc, cols-tc):
+        for y in range(tr, rows-tr):
+            cimage[y, x] = (kernel * image[y-tr:y+tr+1, x-tc:x+tc+1]).sum()
+
+    return cimage
