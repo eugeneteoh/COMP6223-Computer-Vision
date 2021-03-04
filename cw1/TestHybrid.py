706ba8ff74137bf20161d347279b4a35fa02eb78
Eugene Teoh <eugenetwc1@gmail.com>
2021-03-04 22:34:51 +0000

    update

--- cw1/TestHybrid.py
+++ cw1/TestHybrid.py
@@ -2,15 +2,28 @@ import numpy as np
 from scipy.ndimage import gaussian_filter
 import cv2
 from pathlib import Path
+from itertools import permutations
+from multiprocessing import Pool
 
 from MyHybridImages import myHybridImages
 
 file_dir = Path(__file__).resolve().parent
 
-im1 = cv2.imread(str(file_dir/'data/dog.bmp'))
-im2 = cv2.imread(str(file_dir/'data/cat.bmp'))
+im1 = cv2.imread(str(file_dir/'rock.jpeg'))
+im2 = cv2.imread(str(file_dir/'sins.jpeg'))
 
-test = myHybridImages(im1, 16, im2, 8)
-print(np.min(test), np.max(test), np.mean(test))
+def hybrid(sigs):
+    sig1, sig2 = sigs
+    test = myHybridImages(im1, sig1, im2, sig2)
 
-cv2.imwrite(str(file_dir/'hybrid.bmp'), test)
+    cv2.imwrite(str(file_dir/f'output/hybrid_{sig1}_{sig2}.png'), test)
+
+if __name__ == '__main__':
+    with Pool(3) as p:
+        p.map(hybrid, list(permutations(range(1, 21), 2)))
+
+
+# for sig1, sig2 in permutations(range(1, 21), 2):
+    # test = myHybridImages(im1, sig1, im2, sig2)
+    # cv2.imwrite(str(file_dir/f'output/hybrid_{sig1}_{sig2}.png'), test)
+
