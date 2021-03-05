de55ae73979fe38e6369f54ec29d625c7add5df9
Eugene Teoh <eugenetwc1@gmail.com>
2021-03-05 14:12:46 +0000

    hybrid image done

--- cw1/TestConvolution.py
+++ cw1/TestConvolution.py
@@ -7,19 +7,20 @@ iterTest = 10
 
 for i in range(iterTest):
     row, col = np.random.randint(10, 100), np.random.randint(10, 100)
-    testImage = np.random.rand(row, col, 3)
+    # testImage = np.random.rand(row, col, 3)
+    testImage = np.random.rand(row, col)
 
     trow, tcol = np.random.choice([3, 5, 7]), np.random.choice([3, 5, 7])
     testKernel = np.random.randint(-5, 5, size=(trow, tcol))
 
     myconv = convolve(testImage, testKernel)
 
-    myconv = myconv[~np.all(myconv == 0, axis=(1, 2))]
-    myconv = myconv[:, ~np.all(myconv == 0, axis=(0, 2))]
+    # myconv = myconv[~np.all(myconv == 0, axis=(1, 2))]
+    # myconv = myconv[:, ~np.all(myconv == 0, axis=(0, 2))]
     scipyconv = np.zeros(testImage.shape)
-    # scipyconv = convolve2d(testImage, testKernel, mode='valid')
-    for j in range(3):
-        scipyconv[:, :, j] = convolve2d(testImage[:, :, j], testKernel, mode='same', boundary='fill', fillvalue=0)
+    scipyconv = convolve2d(testImage, testKernel, mode='same')
+    # for j in range(3):
+        # scipyconv[:, :, j] = convolve2d(testImage[:, :, j], testKernel, mode='same', boundary='fill', fillvalue=0)
 
     myconv = myconv.astype(np.float32)
     scipyconv = scipyconv.astype(np.float32)
--- cw1/TestHybrid.py
+++ cw1/TestHybrid.py
@@ -9,21 +9,23 @@ from MyHybridImages import myHybridImages
 
 file_dir = Path(__file__).resolve().parent
 
-im1 = cv2.imread(str(file_dir/'rock.jpeg'))
-im2 = cv2.imread(str(file_dir/'sins.jpeg'))
+im1 = cv2.imread(str(file_dir/'trump_real.png'))
+im2 = cv2.imread(str(file_dir/'trump_simpson.png'))
 
 def hybrid(sigs):
     sig1, sig2 = sigs
+
     test = myHybridImages(im1, sig1, im2, sig2)
 
     cv2.imwrite(str(file_dir/f'output/hybrid_{sig1}_{sig2}.png'), test)
 
-if __name__ == '__main__':
-    with Pool(3) as p:
-        p.map(hybrid, list(permutations(range(1, 21), 2)))
 
+# if __name__ == '__main__':
+    # with Pool(3) as p:
+        # p.map(hybrid, list(permutations(range(1, 21), 2)))
 
-# for sig1, sig2 in permutations(range(1, 21), 2):
-    # test = myHybridImages(im1, sig1, im2, sig2)
-    # cv2.imwrite(str(file_dir/f'output/hybrid_{sig1}_{sig2}.png'), test)
+sig1, sig2 = 5, 6
+
+test = myHybridImages(im1, sig1, im2, sig2)
+cv2.imwrite(str(file_dir/f'hybrid_{sig1}_{sig2}.png'), test)
 
